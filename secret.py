128f5c86fc0259e574b9f109727fa7f929a2fd1c
Vlad Freishmidt <vladfreishmidt.webdevelopment@gmail.com>
2025-01-02 13:32:35 +0200

    Implement while loop getting user input

---
+++ secret.py
@@ -0,0 +1,8 @@
+secret = "the secret is number 42"
+password = "A1yandex"
+user_try = ""
+
+while user_try != password:
+    user_try = input("Please type password in: ")
+
+print(secret)
--- todo_app/main.py
+++ todo_app/main.py
@@ -1,5 +1,8 @@
-todos = []
 user_prompt = "Enter a todo:"
 
-todo  = input(user_prompt)
-print(todo)
+todos = []
+
+while True:
+    todo  = input(user_prompt)
+    todos.append(todo)
+    print(todos)
